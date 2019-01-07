ebac4f6c635dfdc1db9a5e514dae332da46da26f
cokoyoh <charlesokoyoh@gmail.com>
2019-01-07 21:14:55 +0300

    Add items to the cart

--- src/components/ProductList.vue
+++ src/components/ProductList.vue
@@ -6,7 +6,7 @@
                 src = "https://i.imgur.com/JfPpwOA.gif">
         <ul v-else>
             <li v-for="product in products">
-                {{product.title}} {{product.price}} - {{product.inventory}}
+                {{product.title}} {{product.price | currency}} - {{product.inventory}}
                 <button @click="addProductToCart(product)">Add to cart</button>
             </li>
         </ul>
